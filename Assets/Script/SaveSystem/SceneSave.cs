6c49576e68904d1b23a68c6edb82e4e35beb0b83
Yuki Tabata <ytabata11@gmail.com>
2022-08-24 10:28:24 +1000

    Beginning to implement the scene preservation state.

---
+++ Assets/Script/SaveSystem/SceneSave.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+
+[System.Serializable]
+public class SceneSave
+{
+    // string ey is an identifier name we choose for this list.
+    public Dictionary<string, List<SceneItem>> listSceneItemDictionary;
+}
