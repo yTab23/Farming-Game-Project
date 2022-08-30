4718ea0de8f499b708d8b45909141f6d13fcf2b4
Yuki Tabata <ytabata11@gmail.com>
2022-08-30 13:28:26 +1000

    Saving the game will preserve the time and date.

--- Assets/Script/SaveSystem/SceneSave.cs
+++ Assets/Script/SaveSystem/SceneSave.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 [System.Serializable]
 public class SceneSave
 {
+    public Dictionary<string, int> intDictionary;
     public Dictionary<string, bool> boolDictionary; // string key is an identifier name we choose for this list.
     // string key is an identifier name we choose for this list.
     public Dictionary<string, string> stringDictionary;
